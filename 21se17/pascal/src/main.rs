07c42c4b038b577e6b967e1f21e998965a99edfb
Elapoidea <bear.grizzaffi@gmail.com>
2021-09-24 11:27:43 -0500

    Added comments and failsafe

--- 21se17/pascal/src/main.rs
+++ 21se17/pascal/src/main.rs
@@ -1,16 +1,25 @@
+// import io library to get inputs, and import the pascal module from the other file
 use std::io::{self, Write};
 mod pascal;
 
 fn main() {
+    // create a base pascal object. this has a single row (the vec provided), but more will be added later
+    // change the vec to adjust how the triangle starts. remember to include the zeroes
     let mut x = pascal::Pascal::new(vec![0_u32, 1, 0]);
 
-    x.extend(int_input("pascal height : ") - 1);
+    // extend our pascal so it's a full triangle
+    let height = int_input("pascal height : ");
+    // if this input will cause an error due to its size, stop the program
+    if height >= 36 { println!("this height will create a triangle which exceeds the confines of u32 (max 35)"); return; }
 
+    x.extend(height - 1);
+
+    // format and display the triangle
     println!();
-
     pascal::display(x);
 }
 
+// this gets an input and turns it into a u32
 fn int_input(context: &str) -> u32 {
     print!("{}", context);
 
--- 21se17/pascal/src/pascal.rs
+++ 21se17/pascal/src/pascal.rs
@@ -1,3 +1,4 @@
+// define the pascal structure.
 pub struct Pascal {
     pub triangle: Vec<Vec<u32>>,
 }
@@ -9,49 +10,57 @@ impl Pascal {
         }
     }
 
-    pub fn extend(&mut self, surplus: u32) {
-        for _ in 0..surplus {
+    pub fn extend(&mut self, growth: u32) {
+        for _ in 0..growth {
             self.next_line()
         }
     }
 
     pub fn next_line(&mut self) {
-        let top_row = &self.triangle[(self.triangle.len() - 1) as usize];
-        let halfway =  (top_row.len() as f32 / 2_f32).ceil() as u32;
-        let mut new_row: Vec<u32> = Vec::with_capacity(top_row.len());
+        let latest_row = &self.triangle[(self.triangle.len() - 1) as usize];
+        let mut new_row: Vec<u32> = Vec::with_capacity(latest_row.len());
 
+        // the point in the latest row which should be considered the halfway
+        let halfway =  (latest_row.len() as f32 / 2_f32).ceil() as u32;
+
+        // make half of the new row
         for i in 1..halfway {
-            new_row.push(top_row[i as usize] + top_row[(i - 1) as usize])
+            new_row.push(latest_row[i as usize] + latest_row[(i - 1) as usize])
         }
 
+        // copy the row on the other side
         for i in 1..halfway {
             new_row.push(new_row[(halfway - i - 1) as usize])
         }
 
-        if top_row[halfway as usize] == top_row[(halfway - 1) as usize] {
-            new_row.insert(halfway as usize - 1, top_row[halfway as usize] * 2);
+        // if the row is even, add one more value into the center of the row
+        if latest_row.len() % 2 == 0 {
+            new_row.insert(halfway as usize - 1, latest_row[halfway as usize] * 2);
         }
 
+        // add the zeroes on the sides
         new_row.insert(0, 0);
         new_row.push(0);
 
+        // add the line to the triangle
         self.triangle.push(new_row.to_vec());
     }
 }
 
+// this displays the triangle in a proportioned way
 pub fn display(x: Pascal) {
     let cell_size: u32 = x.triangle[x.triangle.len() - 1][x.triangle.len() / 2].to_string().len() as u32;
 
-    for i in x.triangle {
-        for j in i {
-            if j == 0 { continue }
+    for row in x.triangle {
+        for value in row {
+            if value == 0 { continue }
 
             print!("{}", {
                 let mut cell = String::from("");
 
-                cell.push_str(&j.to_string());
+                cell.push_str(&value.to_string());
 
-                for _ in 0..cell_size - j.to_string().len() as u32 + 2 {
+                for _ in 0..cell_size - value.to_string().len() as u32 + 2 {
                     cell.push(' ')
                 }
 
